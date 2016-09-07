8da890e0f7c7d032107fa27e534c8564f08fb462
rsmith-finix <rsmith@finixcreative.com>
2016-09-07 13:35:55 -0700

    Results - complete return
    Add updated assets for functional app

--- app.js
+++ app.js
@@ -3,32 +3,89 @@
 var express 	=	require("express"),
 	app 		=	express(),
 	path 		=	require("path"),
+	bodyParser 	= 	require('body-parser'),
 	request 	=	require("request"),
 	jsdom 		=	require("jsdom").jsdom,
 	document 	=	jsdom("<html></html>", {}),
 	window 		=	document.defaultView,
-	$ 			=	require("jquery")(window),
-	searchFor 	=	require("./assets/scripts/search");
+	$ 			=	require("jquery")(window);
 
 // Router
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-	req.on('data', function(){
-		alert("Searching...");
-		search($("#filter").val(), $("#query").val());
-		$("#search-results").html("<p>" + reply + "</p>");
-	});
-	res.write("Form Loading...");
 	res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/search", function(req, res){
+	var params = req.body,
+		filter = params.type,
+		query = params.q,
+		reply = [],
+		list = "",
+		api = "https://api.spotify.com/v1/search?type=" + filter + "&q=" + query,
+		template = {
+			open: `<div class="full">`,
+			close: `</div>`
+		};
+
+	request(api, function(error, response, body){
+		var searchResults = JSON.parse(body);
+		if(!error && response.statusCode == 200){
+			if(filter === "artist"){
+				list = searchResults.artists;
+			} else if(filter === "track"){
+				list = searchResults.tracks;
+			} else if(filter === "album"){
+				list = searchResults.albums;
+			}
+			reply += template.open;
+			for(var i = 0; i < list.items.length; i++){
+				var item = list.items[i];
+				if(item.images[0]){
+					reply +=	`<a class="card" href="${item.external_urls.spotify}" target="_blank" style="background-image: url(${item.images[0].url})">
+									${item.name}
+								</a>`;
+				} else {
+					reply +=	`<a class="card" href="${item.external_urls.spotify}" target="_blank">
+									${item.name}
+								</a>`;
+				}
+			}
+			reply += template.close;
+		}
+		res.end(reply);
+	});
 });
 
 app.listen(8888);
 
-// Test API call
+// Spotify API search
 /*
-searchFor.search("artist", "beatles");
-searchFor.search("track", "hey jude");
-searchFor.search("album", "abbey road");
+function search(filter, query){
+	var api = "https://api.spotify.com/v1/search?type=" + filter + "&q=" + query;
+	request(api, function(error, response, body){
+		var searchResults = JSON.parse(body),
+			reply = [],
+			list;
+		if(!error && response.statusCode == 200){
+			if(filter === "artist"){
+				list = searchResults.artists;
+			} else if(filter === "track"){
+				list = searchResults.tracks;
+			} else if(filter === "album"){
+				list = searchResults.albums;
+			}
+			reply += "<ul>";
+			for(var i = 0; i < list.items.length; i++){
+				reply += `<li>${i + 1} | ${list.items[i].name}</li>`;
+			}
+			reply += "</ul>";
+			console.log(reply);
+		}
+	}).end();
+};
 */
--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,39 +1,15 @@
 // Search
 
-var search = function(filter, query){
-	var api = "https://api.spotify.com/v1/search?type=" + filter + "&q=" + query;
-	// alert(api);
-	$.get(api, function(error, response, body){
-		if(!error && response.statusCode == 200){
-			var searchResults = JSON.parse(body),
-				list,
-				reply = "";
-			if(filter === "artist"){
-				list = searchResults.artists;
-			} else if(filter === "track"){
-				list = searchResults.tracks;
-			} else if(filter === "album"){
-				list = searchResults.albums;
-			}
-			for(var i = 0; i < list.items.length; i++){
-				reply += (i + 1) + " | " + list.items[i].name + "\n";
-			}
-			console.log(reply);
-			$("#search-results").html("<p>" + reply + "</p>");
-			return reply;
-		}
-		return alert(
-			"Error! " + error[0] + " / " + error[1] + "\n" +
-			"Response: " + response.statusCode + "\n" +
-			"Body: " + body[0] + " / " + body[1]
-		);
-	});
-};
-/*
 $("#search-form").on("submit", function(event){
 	event.preventDefault();
-	alert("Searching...");
-	search($("#filter").val(), $("#query").val());
-	$("#search-results").html("<p>" + reply + "</p>");
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8888/search',
+        data: {type: $("#filter").val(), q: $("#query").val()}
+    })
+    .done(function(data){
+		template = `${data}`;
+		console.log("Data: " + data);
+		$("#search-results").html(template);
+	});
 });
-*/
